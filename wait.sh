f62aab3e591e180d994537d2ab9f7be47d35c4d8
Gordon Shankman <gshankman@gmail.com>
2018-02-20 13:26:54 -0500

    Added echo statements to wait.sh

--- wait.sh
+++ wait.sh
@@ -11,11 +11,15 @@ _abort() {
 test -n "${WAIT_HANDLE}" || _abort "WAIT_HANDLE must be provided"
 test -n "${UNIQUE_ID}" || _abort "UNIQUE_ID must be provided"
 
+echo "Sleeping for ${WAIT_TIMEOUT} seconds"
 sleep ${WAIT_TIMEOUT} || _abort "Sleep failed"
 
-curl -v -i -XPUT \
+echo "Wait complete"
+curl -v -i -s -XPUT \
      -H 'Content-Type:' \
      --data-binary "{\"Status\":\"SUCCESS\",\"Reason\":\"Timeout elapsed\",\"UniqueId\":\"${UNIQUE_ID}\",\"Data\":\"Timeout elapsed\"}" \
      ${WAIT_HANDLE} || _abort "Error triggering wait condition!"
+
+echo "Wait condition triggered"
 
 test -n "${KEEP_ALIVE_AFTER_TRIGGER}" && yes >/dev/null
